77c63b0949fede1c54ab033d463db22dca7c08d7
NenadVrtue <hello@vrtuedigital.com>
2021-08-17 13:02:54 +0200

    Company Page
    Company page almost full

---
+++ src/components/values.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import {Link} from "gatsby"
+import { StaticImage } from 'gatsby-plugin-image'
+
+const Values = () => {
+    return (
+        <section className=" bg-gray-100 flex flex-col items-center relative pt-8 xl:pt-14 pb-8 xl:pb-20 overflow-hidden">
+            <h2 className="font-display text-4xl  lg:text-5xl text-main-blue">OUR VALUES</h2>
+             <div className="bg-main-red w-24 h-2 my-7"></div>
+             <p className="font-body font-main max-w-prose pr-2 text-main-blue mb-10 sm:mb-12">We are committed to an exceptional customer experience.</p>
+             
+             <div className=" lg:grid lg:grid-cols-3 gap-x-8 lg:gap-x-16 xl:justify-between xl:gap-x-28">
+                
+                 <div
+                    className="bg-main-blue w-330 flex flex-col items-center px-5 pt-8 pb-9  my-8">
+                    <StaticImage src="../images/family.png"
+                    alt="family icon"
+                    placeholder="tracedSVG"></StaticImage>
+                    <h3 className="text-white font-display text-4xl pt-4 pb-2.5">Family</h3>
+                    <div className="bg-main-red w-24 h-1 mb-4 mt-4"></div>
+                    <p className="text-center font-body text-white leading-6 px-8">We are a family run business and treat everyone like they’re part of 
+                    the family.</p>
+                 </div>
+                 
+                 
+                 <div 
+                    
+                    className="bg-main-blue w-330 flex flex-col items-center px-5 pt-8 pb-9  my-8">
+                    <StaticImage src="../images/integrity.png"
+                    alt="integrity icon"
+                    placeholder="tracedSVG"
+                    ></StaticImage>
+                    <h3 className="text-white font-display text-4xl pt-4 pb-2.5">Integrity</h3>
+                    <div className="bg-main-red w-24 h-1 mb-4 mt-4"></div>
+                    <p className="text-center font-body text-white leading-6">We believe our integrity defines us, and we won’t do anything to damage our trust with you. </p>
+                 </div>
+                 
+                 
+                 
+                 <div 
+                    
+                    className="bg-main-blue w-330 flex flex-col items-center px-5 pt-8 pb-9  my-8">
+                    <StaticImage src="../images/respect.png"
+                    alt="respect icon"
+                    placeholder="tracedSVG"></StaticImage>
+                    <h3 className="text-white font-display text-4xl pt-4 pb-2.5">Teamwork</h3>
+                    <div className="bg-main-red w-24 h-1 mb-4 mt-4"></div>
+                    <p className="text-center font-body text-white leading-6">We are a team of transportation experts, and from drivers to executives, we all work together to ensure your items arrive on time. </p>
+                 </div>
+                 
+                 <div 
+                    
+                    className="bg-main-blue w-330 flex flex-col items-center px-5 pt-8 pb-9  my-8">
+                    <StaticImage src="../images/safety.png"
+                    alt="safety icon"
+                    placeholder="tracedSVG"
+                    ></StaticImage>
+                    <h3 className="text-white font-display text-4xl pt-4 pb-2.5">Safety</h3>
+                    <div className="bg-main-red w-24 h-1 mb-4"></div>
+                    <p className="text-center font-body text-white leading-6">We maintain strict policies and procedures to ensure cargo and employees are safe. </p>
+                 </div>
+
+                <div
+                    
+                    className="bg-main-blue w-330 flex flex-col items-center px-5 pt-8 pb-14  my-8">
+                    <StaticImage src="../images/Customers.png"
+                    alt="customers icon"
+                    placeholder="tracedSVG"></StaticImage>
+                    <h3 className="text-white font-display text-4xl pt-4 pb-2.5">CUSTOMERS</h3>
+                    <div className="bg-main-red w-24 h-1 mb-4"></div>
+                    <p className="text-center font-body text-white leading-6">Customers are always our number one priority, and it’s our goal to make every interaction with our company positive.</p>
+                 </div>
+                 
+                 <div 
+                    
+                    className="bg-main-blue w-330 flex flex-col items-center px-5 pt-8 pb-9  my-8">
+                    <StaticImage src="../images/responsibility.png"
+                    alt="responsibility icon"
+                    height={116}
+                    placeholder="tracedSVG"></StaticImage>
+                    <h3 className="text-white font-display text-4xl pt-4 pb-2.5">responsibility</h3>
+                    <div className="bg-main-red w-24 h-1 mb-4 "></div>
+                    <p className="text-center font-body text-white leading-6">In addition to trucking services, we aim to be a positive part of our community and provide great careers for our employees </p>
+                 </div>
+
+
+
+             </div>
+
+            
+                 
+             
+             
+
+            
+        </section>
+    )
+}
+
+export default Values
--- src/pages/company.js
+++ src/pages/company.js
@@ -2,14 +2,101 @@ import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
 import ComHero from '../components/ComHero'
 import Header from '../components/header'
-
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import Values from '../components/values'
 
 const Company = () => {
     return (
         <Layout>
             <Header></Header>
             <ComHero></ComHero>
+
+            {/* First About Section */}
+
+            <section className='overflow-hidden mb-8  sm:mb-12 md:mt-8 lg:mt-16  flex flex-1 flex-col-reverse md:flex-row gap-x-8 gap-y-4 lg:gap-12 relative'>
+
+                <div className="absolute w-full md:w-8/12 -z-10 h-full bg-gray-100 bottom-0 right-0"></div> 
+                <StaticImage src="../images/About1.png" alt="truck in the about section"
+                placeholder="tracedSVG"
+                className="md:w-2/4 md:my-16"
+                quality="100"
+                
+                />
+            <div className="px-8 flex-col my-12 py-4 md:w-2/4 md:px-0">
+                <h2 data-sal="slide-left"
+                  data-sal-delay="700"
+                  data-sal-easing="ease"
+                  data-sal-duration= "700"
+                  className="font-display text-4xl  lg:text-5xl text-main-blue">
+                    ABOUT US
+                </h2>
+                <div  className="bg-main-red w-24 h-2 my-7"></div>
+                <p data-sal="slide-left"
+                  data-sal-delay="700"
+                  data-sal-easing="ease"
+                 data-sal-duration= "700"
+                   className="font-body font-main max-w-prose pr-2 text-main-blue mb-10 sm:mb-12">
+                    Route Carriers is a trucking company dedicated to providing reliable, affordable and secure transportation and logistics services throughout the United States. Our focus is customer satisfaction from the moment you order our services, all the way to the last box unloaded from our trailers.  We are offering a breath of fresh air in a stale industry that has forgotten about customer service and going above and beyond for their clients.  We are different,and that’s a good thing.
+                </p>
+                 <Link
+                    to="/apply-now"
+                    className="w-48 flex items-center justify-center px-4 py-1 border-4 border-main-red text-sub font-medium font-body text-white bg-main-red hover:bg-main-red-hover hover:border-main-red-hover   "
+                  >
+                    Apply Now
+                  </Link>
+            </div>
+        </section>
+
+        {/* Second About Section */}
+        <section className='overflow-hidden mb-8  sm:mb-12 md:mt-8 lg:mt-16  flex flex-1 flex-col-reverse md:flex-row-reverse gap-x-8 gap-y-4 lg:gap-12 relative '>
+
+                
+                <StaticImage src="../images/About2.png" alt="truck in the about section"
+                placeholder="tracedSVG"
+                className="md:w-2/4 md:my-16"
+                quality="100"
+                
+                />
+            <div className="px-8 flex-col my-12 py-4 md:w-2/4 xl:pl-20 2xl:pl-80 md:px-4  ">
+                <h2 data-sal="slide-right"
+                  data-sal-delay="700"
+                  data-sal-easing="ease"
+                  data-sal-duration= "700"
+                  className="font-display text-4xl  lg:text-5xl text-main-blue">
+                    OUR MISSION
+                </h2>
+                <div  className="bg-main-red w-24 h-2 my-7"></div>
+                <p data-sal="slide-right"
+                  data-sal-delay="700"
+                  data-sal-easing="ease"
+                 data-sal-duration= "700"
+                   className="font-body font-main max-w-prose pr-2 2xl:pr-16 text-main-blue mb-10 sm:mb-12">
+                    Our mission is to delivery our cargo and freight in a safe and timely manner. We are a reliable transportation partner for
+                    your business, and always deliver.
+                </p>
+                 <Link
+                    to="/apply-now"
+                    className="w-48 flex items-center justify-center px-4 py-1 border-4 border-main-red text-sub font-medium font-body text-white bg-main-red hover:bg-main-red-hover hover:border-main-red-hover   "
+                  >
+                    Apply Now
+                  </Link>
+            </div>
+        </section>
+        <Values></Values>
+        {/* CTA */}
+        <section className="py-16 flex flex-col items-center">
+            <h2 className="font-display text-4xl  lg:text-5xl text-main-blue mt-4">
+                JOIN OUR TEAM, we value drivers!
+            </h2>
+             <Link
+                    to="/apply-now"
+                    className=" mt-7 w-48 flex items-center justify-center px-4 py-1 border-4 border-main-red text-sub font-medium font-body text-white bg-main-red hover:bg-main-red-hover hover:border-main-red-hover  mb-8 "
+                  >
+                    Apply Now
+                  </Link>
+        </section>
+
         </Layout>
     )
 }
