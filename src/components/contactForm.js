77752d22e46ba6877c88cfa134313eb45be62054
NenadVrtue <hello@vrtuedigital.com>
2021-08-26 12:01:36 +0200

    error style test

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -43,16 +43,16 @@ const ContactForm = () => {
                 const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
                 const errors = {};
                 if(!values.name) {
-                errors.name = 'Name Required'
+                errors.name = '* Name Required'
                 }
                 if(!values.phone) {
-                errors.phone = 'Phone Required'
+                errors.phone = '* Phone Required'
                 }
                 if(!values.email || !emailRegex.test(values.email)) {
-                errors.email = 'Valid Email Required'
+                errors.email = '* Valid Email Required'
                 } 
                 if(!values.message) {
-                errors.message = 'Message Required'
+                errors.message = '* Message Required'
                 }
                 return errors;
                 }}>
@@ -60,7 +60,7 @@ const ContactForm = () => {
                     <div className="flex flex-col mb-4">
                         <label className="text-base w-full text-body text-main-blue mb-4" htmlFor='name'>Full Name:</label>
                         <Field className=" border-2 border-main-blue px-2 py-2 focus:outline-none focus:border-main-red focus:ring-1 focus:ring-main-red" name='name' />
-                        <ErrorMessage name="name" />
+                        <ErrorMessage component="div" className="text-main-red  mt-2" name="name"  />
                     </div>
 
                     <div className="flex flex-col mb-4">
